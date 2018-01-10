1ebf86e3f8596a3f02b8d775db1eb501705fd1d9
dustinpfister <dustin.pfister@gmail.com>
2018-01-10 18:46:38 -0500

    making a server for the test project

---
+++ server.js
@@ -0,0 +1,73 @@
+let http = require('http'),
+fs = require('fs'),
+path = require('path'),
+port = process.argv[2] || 8080,
+dir_public = 'public';
+
+let forMethod = {
+
+    get: function (req, res) {
+
+        let ext = path.extname(req.url).toLowerCase(),
+        uri = path.join(dir_public, req.url);
+
+        // if no ext then assume index.html of path
+        if (!ext) {
+
+            uri = path.join(dir_public, req.url, 'index.html');
+            ext = '.html';
+
+        }
+
+        fs.readFile(uri, 'utf-8', function (e, data) {
+
+            if (e) {
+
+                res.writeHead(500);
+                res.end(e.message);
+
+            } else {
+
+                // default mime to html
+                let mime = 'text/html';
+                // set plain text for *.js
+                mime = ext === '.js' ? 'text/plain' : mime;
+
+                res.setHeader('Content-Type', mime);
+                res.writeHead(200);
+                res.end(data, 'utf-8');
+
+            }
+
+        });
+    },
+
+    post: function (req, res) {
+
+        res.end('sure');
+
+    }
+
+};
+
+let server = http.createServer(function (req, res) {
+
+        let method = forMethod[req.method.toLowerCase()];
+
+        if (method) {
+
+            method(req, res);
+
+        } else {
+
+            res.end('unsupported method');
+
+        }
+
+    });
+
+server.listen(port, function () {
+
+    console.log('check it on : ' + port);
+
+});
